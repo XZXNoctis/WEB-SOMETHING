d1aa926b82609f5bd55dcb331c4d69505deab30d
XZXNoctis <1321760986@qq.com>
2019-03-16 16:26:42 +0800

    nothing

--- my-project/README.md
+++ my-project/README.md
@@ -1,4 +1,4 @@
-# my-project
+# jd
 
 ## Project setup
 ```
--- my-project/src/views/Home.vue
+++ my-project/src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+	<div id="index">
+		<search-bar></search-bar>
+		<swiper></swiper>
+		
+		<quick-nav></quick-nav>
+		
+		
+		
+		<div class="product-list clear">
+			
+			<product v-for="n in 10"  :id="n" ></product>
+			
+		</div>
+		
+		<tab-bar></tab-bar>
+		<div style="height: 1000px;background-color: yellow;"></div>
+	</div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	import SearchBar from "@/components/SearchBar.vue";
+	import TabBar from "@/components/TabBar.vue";
+	import Swiper from "@/components/Swiper.vue";
+	import QuickNav from "@/components/QuickNav.vue";
+	import Product from "@/components/Product.vue";
+	import axios from "axios";
+	import qs from "qs";
+	
+	
+	
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		data: function() {
+			return {
+				product:[],
+			};
+		},
+		methods: {},
+		props: [],
+		watch: {},
+		computed: {},
+		components: {
+			SearchBar,
+			TabBar,
+			Swiper,
+			QuickNav,
+			Product
+		},
+		created(){
+			axios.get("/product.json",qs,stringify({
+				id:1,
+				user:'admin'
+			}))
+			.then(res => {
+				this.products = res.data;
+			})
+			.catch(err =>{})
+			.finally(()=>{});
+			
+			console.log(qs.stringify({
+				id:1,
+				user:'admin'
+			}));
+		}
+	}
 </script>
+
+<style>
+
+</style>
